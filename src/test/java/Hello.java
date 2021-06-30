f8fdd405ef4778a51e9addf091f5a45dfae4c807
Aziz Niada <niadaaaziz@yahoo.fr>
2021-06-30 19:32:15 -0400

    Addind more info on commit 2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("second commit");
+        System.out.println("second commit. Added more info on second commit");
     }
 }
